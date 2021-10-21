b04ff18829fc770244e43f2fc86a69e21fc715b8
anil kumar <anilkumarn3@gmail.com>
2021-10-21 20:03:57 +0530

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmeTHpN3gi6vgHGsjO2w3idbeSjq3f-aM",
+  authDomain: "netflix-clone-f1186.firebaseapp.com",
+  projectId: "netflix-clone-f1186",
+  storageBucket: "netflix-clone-f1186.appspot.com",
+  messagingSenderId: "1061556510532",
+  appId: "1:1061556510532:web:811df8611e0d1e80164111",
+};
